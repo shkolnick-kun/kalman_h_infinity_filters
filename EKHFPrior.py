24277679d3be4eaae6e8c940da65ec0c141e7611
shkolnick-kun <shkolnick.kun@gmail.com>
2019-09-15 18:15:03 +0500

    Added files

---
+++ EKHFPrior.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name,too-many-instance-attributes, too-many-arguments
+"""
+Copyright 2019 Paul A Beltyukov
+Copyright 2015 Roger R Labbe Jr.
+
+FilterPy library.
+http://github.com/rlabbe/filterpy
+
+Documentation at:
+https://filterpy.readthedocs.org
+
+Supporting book at:
+https://github.com/rlabbe/Kalman-and-Bayesian-Filters-in-Python
+
+This is licensed under an MIT license. See the readme.MD file
+for more information.
+"""
+from copy import deepcopy
+import numpy as np
+from numpy import dot, eye
+from scipy.stats import chi2
+import scipy.linalg as linalg
+from filterpy.kalman import ExtendedKalmanFilter
+
+class ExtendedKalmanHinfFilterPrior(ExtendedKalmanFilter):
+    def __init__(self, dim_x, dim_z, dim_u=0, alpha = 0.01):
+        ExtendedKalmanFilter.__init__(self, dim_x, dim_z, dim_u)
+        self.beta_n = chi2.ppf(1.0 - alpha, dim_z)
+        
+    def update(self, z, HJacobian, Hx, R=None, args=(), hx_args=(),
+               residual=np.subtract):
+        """ Performs the update innovation of the extended Kalman/Hinfinity 
+        filter with prior residuals used for Hinfinity correction.
+
+        Parameters
+        ----------
+
+        z : np.array
+            measurement for this step.
+            If `None`, posterior is not computed
+
+        HJacobian : function
+           function which computes the Jacobian of the H matrix (measurement
+           function). Takes state variable (self.x) as input, returns H.
+
+        Hx : function
+            function which takes as input the state variable (self.x) along
+            with the optional arguments in hx_args, and returns the measurement
+            that would correspond to that state.
+
+        R : np.array, scalar, or None
+            Optionally provide R to override the measurement noise for this
+            one call, otherwise  self.R will be used.
+
+        args : tuple, optional, default (,)
+            arguments to be passed into HJacobian after the required state
+            variable. for robot localization you might need to pass in
+            information about the map and time of day, so you might have
+            `args=(map_data, time)`, where the signature of HCacobian will
+            be `def HJacobian(x, map, t)`
+
+        hx_args : tuple, optional, default (,)
+            arguments to be passed into Hx function after the required state
+            variable.
+
+        residual : function (z, z2), optional
+            Optional function that computes the residual (difference) between
+            the two measurement vectors. If you do not provide this, then the
+            built in minus operator will be used. You will normally want to use
+            the built in unless your residual computation is nonlinear (for
+            example, if they are angles)
+        """
+        if z is None:
+            self.z = np.array([[None]*self.dim_z]).T
+            self.x_post = self.x.copy()
+            self.P_post = self.P.copy()
+            return
+
+        if not isinstance(args, tuple):
+            args = (args,)
+
+        if not isinstance(hx_args, tuple):
+            hx_args = (hx_args,)
+
+        if R is None:
+            R = self.R
+        elif np.isscalar(R):
+            R = eye(self.dim_z) * R
+
+        if np.isscalar(z) and self.dim_z == 1:
+            z = np.asarray([z], float)
+            
+        #Will check for folter Divergence
+        H = HJacobian(self.x, *args)
+        hx = Hx(self.x, *hx_args)
+        nu = residual(z, hx)
+
+        PHT = self.P.dot(H.T)
+        C = H.dot(PHT)
+        self.S = C + R
+        self.SI = linalg.inv(self.S)
+        
+        thr = self.beta_n
+        if dot(nu.T, dot(self.SI, nu)) >= thr: 
+            #Divergence detected, H-infinity correction needed
+            A = np.outer(nu, nu.T)/thr - self.S
+            D = PHT.dot(linalg.pinv(C))
+            self.P += D.dot(A.dot(D.T))
+            #Need to recompute PHT, self.S and self.SI due to self.P update
+            PHT = dot(self.P, H.T)
+            self.S = dot(H, PHT) + R
+            self.SI = linalg.inv(self.S)
+        
+        #Now we may update self.K, self.P, self.y, self.x
+        self.y = nu
+        self.K = PHT.dot(self.SI)
+        self.x += dot(self.K, nu)
+        
+        # P = (I-KH)P(I-KH)' + KRK' is more numerically stable
+        # and works for non-optimal K vs the equation
+        # P = (I-KH)P usually seen in the literature.
+        I_KH = self._I - dot(self.K, H)
+        self.P = dot(I_KH, self.P).dot(I_KH.T) + dot(self.K, R).dot(self.K.T)
+        
+        # set to None to force recompute
+        self._log_likelihood = None
+        self._likelihood = None
+        self._mahalanobis = None
+
+        # save measurement and posterior state
+        self.z = deepcopy(z)
+        self.x_post = self.x.copy()
+        self.P_post = self.P.copy()
+        
+    def predict_update(self, z, HJacobian, Hx, args=(), hx_args=(), u=0):
+        """ Performs the predict/update innovation of the extended Kalman
+        filter.
+
+        Parameters
+        ----------
+
+        z : np.array
+            measurement for this step.
+            If `None`, only predict step is perfomed.
+
+        HJacobian : function
+           function which computes the Jacobian of the H matrix (measurement
+           function). Takes state variable (self.x) as input, along with the
+           optional arguments in args, and returns H.
+
+        Hx : function
+            function which takes as input the state variable (self.x) along
+            with the optional arguments in hx_args, and returns the measurement
+            that would correspond to that state.
+
+        args : tuple, optional, default (,)
+            arguments to be passed into HJacobian after the required state
+            variable.
+
+        hx_args : tuple, optional, default (,)
+            arguments to be passed into Hx after the required state
+            variable.
+
+        u : np.array or scalar
+            optional control vector input to the filter.
+        """
+        self.predict(u)
+        self.update(z, HJacobian, Hx, self.R, args, hx_args, residual=np.subtract)    
---
+++ UDEKHFPost.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name,too-many-instance-attributes, too-many-arguments
+"""
+Copyright 2019 Paul A Beltyukov
+Copyright 2015 Roger R Labbe Jr.
+
+FilterPy library.
+http://github.com/rlabbe/filterpy
+
+Documentation at:
+https://filterpy.readthedocs.org
+
+Supporting book at:
+https://github.com/rlabbe/Kalman-and-Bayesian-Filters-in-Python
+
+This is licensed under an MIT license. See the readme.MD file
+for more information.
+"""
+import numpy as np
+from numpy import dot, outer, diag
+from scipy.stats import chi2
+from UDEKF import mwgs, UDExtendedKalmanFilter
+
+
+class UDExtendedKalmanHinfFilterPosterior(UDExtendedKalmanFilter):
+
+    """ 
+    Implements an UD modification of extended Kalman/Hinfinity filter (EKHF)
+    with posterior resuduals used for Hinfinity correction. You are 
+    responsible for setting the various state variables to reasonable values; 
+    the defaults will  not give you a functional filter.
+
+    You will have to set the following attributes after constructing this
+    object for the filter to perform properly. Please note that there are
+    various checks in place to ensure that you have made everything the
+    'correct' size. However, it is possible to provide incorrectly sized
+    arrays such that the linear algebra can not perform an operation.
+    It can also fail silently - you can end up with matrices of a size that
+    allows the linear algebra to work, but are the wrong shape for the problem
+    you are trying to solve.
+
+    Parameters
+    ----------
+
+    dim_x : int
+        Number of state variables for the Kalman filter. For example, if
+        you are tracking the position and velocity of an object in two
+        dimensions, dim_x would be 4.
+
+        This is used to set the default size of P, Q, and u
+
+    dim_z : int
+        Number of of measurement inputs. For example, if the sensor
+        provides you with position in (x,y), dim_z would be 2.
+
+    Attributes
+    ----------
+    x : numpy.array(dim_x, 1)
+        State estimate vector
+
+    P : numpy.array(dim_x, dim_x)
+        Covariance matrix
+
+    x_prior : numpy.array(dim_x, 1)
+        Prior (predicted) state estimate. The *_prior and *_post attributes
+        are for convienence; they store the  prior and posterior of the
+        current epoch. Read Only.
+
+    P_prior : numpy.array(dim_x, dim_x)
+        Prior (predicted) state covariance matrix. Read Only.
+
+    x_post : numpy.array(dim_x, 1)
+        Posterior (updated) state estimate. Read Only.
+
+    P_post : numpy.array(dim_x, dim_x)
+        Posterior (updated) state covariance matrix. Read Only.
+
+    R : numpy.array(dim_z, dim_z)
+        Measurement noise matrix
+
+    Q : numpy.array(dim_x, dim_x)
+        Process noise matrix
+
+    F : numpy.array()
+        State Transition matrix
+
+    H : numpy.array(dim_x, dim_x)
+        Measurement function
+
+    y : numpy.array
+        Residual of the update step. Read only.
+
+    K : numpy.array(dim_x, dim_z)
+        Kalman gain of the update step. Read only.
+
+    S :  numpy.array
+        Systen uncertaintly projected to measurement space. Read only.
+
+    z : ndarray
+        Last measurement used in update(). Read only.
+
+    log_likelihood : float
+        log-likelihood of the last measurement. Read only.
+
+    likelihood : float
+        likelihood of last measurment. Read only.
+
+        Computed from the log-likelihood. The log-likelihood can be very
+        small,  meaning a large negative value such as -28000. Taking the
+        exp() of that results in 0.0, which can break typical algorithms
+        which multiply by this value, so by default we always return a
+        number >= sys.float_info.min.
+
+    mahalanobis : float
+        mahalanobis distance of the innovation. E.g. 3 means measurement
+        was 3 standard deviations away from the predicted value.
+
+        Read only.
+    """
+    
+    def __init__(self, dim_x, dim_z, dim_u=0, alpha = 0.01):
+        UDExtendedKalmanFilter.__init__(self, dim_x, dim_z, dim_u)
+        self.beta_1 = chi2.ppf(1.0 - alpha, 1)
+        
+    def _scalar_update(self, axis_residual, axis_hjacobian, r):
+        """Joseph/Hinfinity scalar update using prior errors
+        
+        Parameters
+        ----------
+        
+        axis_residual : function which returns current axis residual 
+                        returns scalar, float.
+        
+        axis_hjacobian : function which returns current axis HJacobian row 
+                         returns np.array, float.
+        
+        r : scalar, float, current axis state disp
+        """
+        u, d, x, n = self.U, self.D, self.x, self.dim_x
+        nu = axis_residual(x)
+        h  = axis_hjacobian(x)
+        
+        #Joseph update
+        f = h.dot(u)
+        v = d * f
+        s = f.dot(v) + r 
+        
+        K = u.dot(v / s).reshape((n, 1))
+    
+        WW = np.concatenate((outer(K, f) - u, K), axis = 1)
+        DD = np.concatenate((d, np.array([r])))
+    
+        u,d = mwgs(WW, DD)
+        x += (K*nu).reshape(x.shape)
+        
+        #Will check for divergence
+        eta = axis_residual(x)
+        h_hat  = axis_hjacobian(x)
+        
+        f = h_hat.dot(u)
+        v = d * f
+        s = f.dot(v) + r
+        
+        a = (eta * eta) / self.beta_1 - s
+        if a >= 0.0:
+            #Divergence detected, H-infinity correction needed
+            h_tilde = h_hat - h_hat.dot(K)*h
+            f_tilde = h_tilde.dot(u)
+            v_tilde = d * f_tilde
+            
+            c = f.dot(v_tilde)
+            q = a/(c*c)
+            WW = np.concatenate((u, dot(u, v_tilde).reshape((n,1))), axis = 1)
+            DD = np.concatenate((d, np.array([q])))
+            u, d = mwgs(WW, DD)
+            #Now must recompute x-update
+            K = dot(u, dot(diag(d), dot(u, h))) / r
+            x = self.x + (K*nu).reshape(x.shape)
+    
+        self.U, self.D, self.x = u, d, x
